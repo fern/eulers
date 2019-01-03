c643b91916b44a0444a5fa4154edbb1f91511c0b
Fernando Padilla <fern@alum.mit.edu>
2019-01-03 14:30:06 -0800

    initial project

---
+++ src/main/java/com/example/fern/eulers/App.java
@@ -0,0 +1,13 @@
+package com.example.fern.eulers;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
